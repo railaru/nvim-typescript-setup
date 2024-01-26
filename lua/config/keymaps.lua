a48bf95d57c63eaf35d8771d2f7f80f2da61b871
Rūtenis Raila <rutenisraila@hotmail.com>
2024-01-26 14:07:22 +0100

    Update keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,4 +3,7 @@
 -- Add any additional keymaps here
 --
 
+-- shift + f = open telescope to find files
 vim.api.nvim_set_keymap("n", "<S-f>", "<cmd>Telescope find_files<CR>", { noremap = true, silent = true })
+-- ctrl + s = save file, same as w + q
+vim.api.nvim_set_keymap("n", "<D-s>", ":w<CR>", { noremap = true, silent = true })
