6e1e2f906053a926c6879712c77869c79bc245b7
Rūtenis Raila <rutenisraila@hotmail.com>
2024-09-27 10:48:04 +0300

    remove github theme

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -30,8 +30,8 @@ require("lazy").setup({
         version = false, -- always use the latest git commit
         -- version = "*", -- try installing the latest stable version for plugins that support semver
     },
-    install = { colorscheme = { "tokyonight", "habamax", "github_dark" } },
     checker = { enabled = true }, -- automatically check for plugin updates
+    install = { colorscheme = { "habamax" } },
     performance = {
         rtp = {
             -- disable some rtp plugins
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,4 @@
 return {
-    { "projekt0n/github-nvim-theme" },
     -- add gruvbox
     { "ellisonleao/gruvbox.nvim" },
-
-    -- Configure LazyVim to load gruvbox
-    {
-        "LazyVim/LazyVim",
-        opts = {
-            colorscheme = "github_dark_tritanopia",
-        },
-    },
 }
